490c1c632687c948bf4d63fbbde0a2bb3450fed8
Katalina Caldera <katalinacaldera19@gmail.com>
2024-05-20 13:29:39 -0500

    added dropdown and code for the location

--- scripts/national_parks.js
+++ scripts/national_parks.js
@@ -2,7 +2,44 @@
 
 window.onload = () => {
 
-    /*
+    // grabed the radio by id from html
+    let locationRadio = document.querySelector("#location");
+    let typeRadio = document.querySelector("#type");
+
+    locationRadio.addEventListener("change", selectOptions);
+    typeRadio.addEventListener("change", selectOptions);
+    
+
+
+ 
+}
+function selectOptions(){
+
+   let locationOptions = document.querySelector("#options");
+   locationOptions.innerHTML =""
+
+   if(document.querySelector("#location").checked){
+    let locations = locationsArray;
+
+    locations.forEach((location)=>{
+        let option = document.createElement("option");
+        option.textContent = location;
+        locationOptions.appendChild(option);
+    });
+   }else if(document.querySelector("#type").checked){
+
+   }
+
+        
+}
+
+
+
+ 
+
+
+
+/*
         You can remove the following console.log() lines.
         They are here to verify that we have access to the data
         The data script files are located in the scripts/data directory
@@ -16,7 +53,3 @@ window.onload = () => {
 
     //log the nationalParksArray to the console (scripts/data/nationalParkData.js)
     console.log(nationalParksArray)
-
-
-
-}
