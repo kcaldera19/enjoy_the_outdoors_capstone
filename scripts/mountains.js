b0cf41bfad23b5e8eceb7f2153af6783c13a4b8c
Katalina Caldera <katalinacaldera19@gmail.com>
2024-05-21 22:12:02 -0500

    added a carousel to index.html

--- scripts/mountains.js
+++ scripts/mountains.js
@@ -20,7 +20,7 @@ window.onload = () => {
 function displayMountainCard(event){
     let mountainDiv = document.querySelector("#mountainSelected");
     mountainDiv.innerHTML = "";
-    let mountain = mountainsArray.find((mountain)=>mountainsArray.name === event.target.value);
+    let mountain = mountainsArray.find((mountain)=>mountain.name === event.target.value);
 
     let cardDiv = document.createElement("div");
     cardDiv.classList.add("card","w-25");
@@ -30,13 +30,13 @@ function displayMountainCard(event){
 
 
     // need to src properly
-    cardImage.src = mountain.image;
+    cardImage.src =mountain.image;
     cardImage.alt = mountain.name;
 
     cardDiv.appendChild(cardImage);
 
     let cardBody = document.createElement("div");
-    cardDiv.classList.add("card-body");
+    cardBody.classList.add("card-body");
 
     let cardTitle = document.createElement("h5");
     cardTitle.classList.add("card-title");
@@ -52,13 +52,14 @@ function displayMountainCard(event){
 
     cardBody.appendChild(cardText);
 
-    cardDiv.append(cardBody);
+    cardDiv.appendChild(cardBody);
 
     mountainDiv.appendChild(cardDiv);
 
 
 
 }
+
 
 // the dropdown
 function initMountainDropdown(){
@@ -82,3 +83,4 @@ function initMountainDropdown(){
 
 
 }
+console.log(mountainsArray);
